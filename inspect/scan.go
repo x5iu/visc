1605ffe7fcdcb53f8f7484a6f96451436ec35986
Hei <xuboyu72@gmail.com>
2023-07-15 13:24:39 +0800

    🚀 使用静态代码分析重构了 getter/setter 的生成方式

--- inspect/scan.go
+++ inspect/scan.go
@@ -2,30 +2,42 @@ package inspect
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"go/ast"
+	"go/build"
+	"go/importer"
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"go/types"
 	"io/fs"
 	"os"
-	"path"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
 type Package struct {
+	fset    *token.FileSet
 	Name    string
+	Imports []*Import
 	Targets []*Type
 }
 
+type Import struct {
+	Name string
+	Path string
+}
+
 type Type struct {
-	FileSet *token.FileSet
-	Spec    *ast.TypeSpec
+	Fset *token.FileSet
+	Spec *ast.TypeSpec
 }
 
 func (t *Type) String() string {
 	var buf bytes.Buffer
-	printer.Fprint(&buf, t.FileSet, t.Spec)
+	printer.Fprint(&buf, t.Fset, t.Spec)
 	spec := buf.String()
 	spec = spec[t.Spec.Name.Pos()-t.Spec.Pos() : t.Spec.Name.End()-t.Spec.Pos()]
 	if t.Spec.TypeParams != nil {
@@ -41,12 +53,12 @@ func (t *Type) String() string {
 }
 
 func Scan(dir string, files []string) (*Package, error) {
-	if !path.IsAbs(dir) {
+	if !filepath.IsAbs(dir) {
 		pwd, err := os.Getwd()
 		if err != nil {
 			return nil, err
 		}
-		dir = path.Join(pwd, dir)
+		dir = filepath.Join(pwd, dir)
 	}
 	fset := token.NewFileSet()
 	packages, err := parser.ParseDir(fset, dir, filter(files), 0)
@@ -57,21 +69,79 @@ func Scan(dir string, files []string) (*Package, error) {
 		return nil, fmt.Errorf("%d packages found", l)
 	}
 	out := &Package{
-		Targets: make([]*Type, 0, 10),
+		fset:    fset,
+		Imports: make([]*Import, 0, 8),
+		Targets: make([]*Type, 0, 8),
 	}
 	for _, p := range packages {
+		astFiles := make([]*ast.File, 0, len(p.Files))
+		for _, file := range p.Files {
+			astFiles = append(astFiles, file)
+		}
+		conf := types.Config{
+			IgnoreFuncBodies: true,
+			FakeImportC:      true,
+			Importer: &Importer{
+				imported:      map[string]*types.Package{},
+				tokenFileSet:  fset,
+				defaultImport: importer.Default(),
+			},
+		}
+		info := &types.Info{
+			Types:      map[ast.Expr]types.TypeAndValue{},
+			Instances:  map[*ast.Ident]types.Instance{},
+			Defs:       map[*ast.Ident]types.Object{},
+			Uses:       map[*ast.Ident]types.Object{},
+			Implicits:  map[ast.Node]types.Object{},
+			Selections: map[*ast.SelectorExpr]*types.Selection{},
+			Scopes:     map[ast.Node]*types.Scope{},
+			InitOrder:  []*types.Initializer{},
+		}
+		_, err = conf.Check(p.Name, fset, astFiles, info)
+		if err != nil {
+			return nil, err
+		}
+		imports := make(map[string]*ast.Ident, 8)
+		imported := make(map[string]struct{})
 		ast.Inspect(p, func(input ast.Node) bool {
 			switch node := input.(type) {
 			case *ast.Package:
 				out.Name = node.Name
+				return true
+			case *ast.ImportSpec:
+				path, _ := strconv.Unquote(node.Path.Value)
+				imports[path] = node.Name
 				return true
 			case *ast.File, *ast.GenDecl:
 				return true
 			case *ast.TypeSpec:
 				if _, ok := node.Type.(*ast.StructType); ok {
 					out.Targets = append(out.Targets, &Type{
-						FileSet: fset,
-						Spec:    node,
+						Fset: fset,
+						Spec: node,
+					})
+					ast.Inspect(node.Type, func(structType ast.Node) bool {
+						switch expr := structType.(type) {
+						case ast.Expr:
+							if named, ok := info.TypeOf(expr).(*types.Named); ok {
+								if objPkg := named.Obj().Pkg(); objPkg != nil {
+									if alias, exists := imports[objPkg.Path()]; exists {
+										if _, duplicated := imported[objPkg.Path()]; !duplicated {
+											var name string
+											if alias != nil {
+												name = alias.String()
+											}
+											out.Imports = append(out.Imports, &Import{
+												Name: name,
+												Path: objPkg.Path(),
+											})
+											imported[objPkg.Path()] = struct{}{}
+										}
+									}
+								}
+							}
+						}
+						return true
 					})
 				}
 				return false
@@ -88,10 +158,69 @@ func filter(files []string) func(info fs.FileInfo) bool {
 			return true
 		}
 		for _, file := range files {
-			if info.Name() == path.Base(file) {
+			if info.Name() == filepath.Base(file) {
 				return true
 			}
 		}
 		return false
 	}
+}
+
+type Importer struct {
+	imported      map[string]*types.Package
+	tokenFileSet  *token.FileSet
+	defaultImport types.Importer
+}
+
+var importing types.Package
+
+func (importer *Importer) ImportFrom(path, dir string, _ types.ImportMode) (*types.Package, error) {
+	if path == "unsafe" {
+		return types.Unsafe, nil
+	}
+	if path == "C" {
+		return importer.defaultImport.Import("C")
+	}
+	goroot := filepath.Join(build.Default.GOROOT, "src")
+	if _, err := os.Stat(filepath.Join(goroot, path)); err != nil {
+		if os.IsNotExist(err) {
+			target := importer.imported[path]
+			if target != nil {
+				if target == &importing {
+					return nil, errors.New("cycle importing " + path)
+				}
+				return target, nil
+			}
+			importer.imported[path] = &importing
+			pkg, err := build.Import(path, dir, 0)
+			if err != nil {
+				return nil, err
+			}
+			var files []*ast.File
+			for _, name := range append(pkg.GoFiles, pkg.CgoFiles...) {
+				name = filepath.Join(pkg.Dir, name)
+				file, err := parser.ParseFile(importer.tokenFileSet, name, nil, 0)
+				if err != nil {
+					return nil, err
+				}
+				files = append(files, file)
+			}
+			conf := types.Config{
+				Importer:         importer,
+				FakeImportC:      true,
+				IgnoreFuncBodies: true,
+			}
+			target, err = conf.Check(path, importer.tokenFileSet, files, nil)
+			if err != nil {
+				return nil, err
+			}
+			importer.imported[path] = target
+			return target, nil
+		}
+	}
+	return importer.defaultImport.Import(path)
+}
+
+func (importer *Importer) Import(path string) (*types.Package, error) {
+	return importer.ImportFrom(path, "", 0)
 }
--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/x5iu/visc/inspect"
+	"go/ast"
+	"go/format"
+	"go/token"
+	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path"
+	"path/filepath"
+	"reflect"
+	"strings"
 	"text/template"
-	"unicode"
 
 	_ "embed"
 )
 
-const Version = "v0.1.1"
+const Version = "v0.2.0"
 
 //go:embed gen.tmpl
 var genTemplate string
@@ -25,10 +30,8 @@ const (
 
 var (
 	buildTags = flag.String("buildtags", "", "tags attached to output file")
-	genTags   = flag.String("gentags", "", "tags when executing \"go run visc.*.go\"")
 	output    = flag.String("output", "visc.gen.go", "output file")
 	version   = flag.Bool("version", false, "visc version")
-	keepTemp  = flag.Bool("keeptemp", false, "keep temp visc.*.go file")
 )
 
 func init() {
@@ -49,87 +52,167 @@ func main() {
 		log.Fatalln("error: visc should work with //go:generate command")
 	}
 
-	dir := path.Dir(file)
+	dir := filepath.Dir(file)
 	pkg, err := inspect.Scan(dir, flag.Args())
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	if pkg.Name == "main" {
-		log.Fatalln("error: visc does not work on main package")
+	var code bytes.Buffer
+	if err = template.Must(
+		template.New("visc").
+			Funcs(template.FuncMap{
+				"genGetterSetter": genGetterSetter,
+			}).
+			Parse(genTemplate),
+	).Execute(&code, &TaggedPackage{pkg, *buildTags}); err != nil {
+		log.Fatalln(err)
 	}
 
-	ctx := new(Context)
-	ctx.Output = *output
-	ctx.Generics = make(map[string]struct{})
-	ctx.Package.Tags = *buildTags
-	ctx.Package.Name = pkg.Name
-	ctx.Package.Path, err = inspect.GetPackagePath(dir)
+	formatted, err := format.Source(code.Bytes())
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	for _, target := range pkg.Targets {
-		if target.Spec.TypeParams != nil {
-			for _, field := range target.Spec.TypeParams.List {
-				for _, name := range field.Names {
-					ctx.Generics[name.String()] = struct{}{}
+	if err = os.WriteFile(*output, formatted, 0644); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type TaggedPackage struct {
+	*inspect.Package
+	Tag string
+}
+
+func genGetterSetter(t *inspect.Type) string {
+	var w strings.Builder
+	receiver := t.String()
+	for _, field := range t.Spec.Type.(*ast.StructType).Fields.List {
+		for _, name := range field.Names {
+			var tag string
+			if lit := field.Tag; lit != nil {
+				tag = lit.Value[1 : len(lit.Value)-1]
+			}
+			getter, hasGetter, isRef, setter, hasSetter := inspectField(
+				name.String(),
+				reflect.StructTag(tag),
+			)
+			if hasGetter {
+				genFieldGetter(&w, receiver, getter, name.String(), toString(t.Fset, field.Type), isRef)
+			}
+			if hasSetter {
+				genFieldSetter(&w, receiver, setter, name.String(), toString(t.Fset, field.Type))
+			}
+		}
+	}
+	return w.String()
+}
+
+func inspectField(name string, tag reflect.StructTag) (getter string, hasGetter bool, isRef bool, setter string, hasSetter bool) {
+	// inspect getter
+	if getterTag, ok := tag.Lookup("getter"); ok {
+		getterParts := strings.Split(getterTag, ",")
+		if firstPart := strings.TrimSpace(getterParts[0]); firstPart != "" && firstPart != "-" {
+			hasGetter = true
+			if firstPart == "*" {
+				getter = toCamel(name)
+			} else {
+				getter = firstPart
+			}
+			if len(getterParts) > 1 {
+				for _, part := range getterParts[1:] {
+					switch strings.TrimSpace(part) {
+					case "ptr", "pointer", "ref", "reference":
+						isRef = true
+					}
 				}
 			}
 		}
-		targetType := target.String()
-		if len(targetType) > 0 && unicode.IsUpper([]rune(targetType)[0]) {
-			ctx.Types = append(ctx.Types, targetType)
+	}
+
+	// inspect setter
+	if setterTag, ok := tag.Lookup("setter"); ok {
+		setterParts := strings.Split(setterTag, ",")
+		if firstPart := strings.TrimSpace(setterParts[0]); firstPart != "" && firstPart != "-" {
+			hasSetter = true
+			if firstPart == "*" {
+				setter = "Set" + toCamel(name)
+			} else {
+				setter = firstPart
+			}
 		}
 	}
 
-	if err = ctx.Execute(dir); err != nil {
+	return
+}
+
+// Converts a string to CamelCase
+func toCamel(s string) string {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return s
+	}
+
+	n := strings.Builder{}
+	n.Grow(len(s))
+	capNext := true
+	for i, v := range []byte(s) {
+		vIsCap := v >= 'A' && v <= 'Z'
+		vIsLow := v >= 'a' && v <= 'z'
+		if capNext {
+			if vIsLow {
+				v += 'A'
+				v -= 'a'
+			}
+		} else if i == 0 {
+			if vIsCap {
+				v += 'a'
+				v -= 'A'
+			}
+		}
+		if vIsCap || vIsLow {
+			n.WriteByte(v)
+			capNext = false
+		} else if vIsNum := v >= '0' && v <= '9'; vIsNum {
+			n.WriteByte(v)
+			capNext = true
+		} else {
+			capNext = v == '_' || v == ' ' || v == '-' || v == '.'
+		}
+	}
+	return n.String()
+}
+
+func genFieldGetter(w io.Writer, receiver string, method string, field string, typ string, isRef bool) {
+	fmt.Fprintf(w, "func (instance *%s) %s() %s%s { return %sinstance.%s }\n",
+		receiver, method, refType(isRef), typ, refValue(isRef), field)
+}
+
+func refType(isRef bool) string {
+	if isRef {
+		return "*"
+	} else {
+		return ""
+	}
+}
+
+func refValue(isRef bool) string {
+	if isRef {
+		return "&"
+	} else {
+		return ""
+	}
+}
+
+func genFieldSetter(w io.Writer, receiver string, method string, field string, typ string) {
+	fmt.Fprintf(w, "func (instance *%s) %s(value %s) { instance.%s = value }\n",
+		receiver, method, typ, field)
+}
+
+func toString(fset *token.FileSet, node ast.Node) string {
+	var buf strings.Builder
+	if err := format.Node(&buf, fset, node); err != nil {
 		log.Fatalln(err)
 	}
-}
-
-type Context struct {
-	Package struct {
-		Name string
-		Path string
-		Tags string
-	}
-	Generics map[string]struct{}
-	Types    []string
-	Output   string
-}
-
-func (ctx *Context) Execute(dir string) error {
-	temp, err := os.CreateTemp(dir, "visc.*.go")
-	if err != nil {
-		return fmt.Errorf("os.CreateTemp: %w", err)
-	}
-
-	if !*keepTemp {
-		defer os.Remove(temp.Name())
-	}
-
-	defer temp.Close()
-
-	if err = template.Must(template.New("visc").Parse(genTemplate)).
-		Execute(temp, ctx); err != nil {
-		return fmt.Errorf("templte.Execute: %w", err)
-	}
-
-	args := make([]string, 0, 5)
-	args = append(args, "run")
-	if *genTags != "" {
-		args = append(args, "-tags", *genTags)
-	}
-	args = append(args, path.Base(temp.Name()))
-
-	command := exec.Command("go", args...)
-	command.Dir = path.Dir(temp.Name())
-	command.Stdout = os.Stdout
-	command.Stderr = os.Stderr
-	if err = command.Run(); err != nil {
-		return fmt.Errorf("command.Run: %w", err)
-	}
-
-	return nil
+	return buf.String()
 }
